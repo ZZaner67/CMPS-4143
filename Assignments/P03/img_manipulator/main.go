954149aa98982e5bcb704d131ee8354000f339fa
Zane <143670272+ZZaner67@users.noreply.github.com>
2023-12-07 16:31:00 -0600

    Add files via upload

---
+++ Assignments/P03/img_manipulator/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/ZZaner67/GoLang_Modules/imgcolors"
+	"github.com/ZZaner67/GoLang_Modules/imgget"
+	"github.com/ZZaner67/GoLang_Modules/imggrayscale"
+	"github.com/ZZaner67/GoLang_Modules/imgtext"
+)
+
+func main() {
+
+	GetPic.GetPic()
+	Text.Text()
+	Grayscale.Grayscale()
+	Colors.Colors()
+}
